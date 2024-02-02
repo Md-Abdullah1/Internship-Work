3cfe0120ad34d604db1e4cf4623ba57d6b4bad2a
Md-Abdullah1 <abdullahahil153@gmail.com>
2024-02-02 18:02:11 +0530

    stared with PWA app

---
+++ fisrt-pwa/src/App.js
@@ -0,0 +1,23 @@
+// import logo from './logo.svg';
+// import { Button } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar bg="primary" data-bs-theme="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
+  );
+}
+
+export default App;
