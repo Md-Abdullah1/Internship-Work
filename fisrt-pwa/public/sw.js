74043936b45bad4ffacdf3ca900068715584e554
Md-Abdullah1 <abdullahahil153@gmail.com>
2024-02-07 09:55:05 +0530

    added offline storage in app

--- fisrt-pwa/public/sw.js
+++ fisrt-pwa/public/sw.js
@@ -1,31 +1,39 @@
 let cacheData = "AppV1";
-this.addEventListener("install",(event)=>{
+this.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(cacheData).then((cache) => {
+      return cache.addAll([
+        "/static/js/main.chunk.js",
+        "/static/js/0.chunk.js",
+        "/static/js/bundle.js",
+        "/index.html",
+        "/",
+        "/about",
+        "/users",
+      ]);
+    })
+  );
+});
+
+this.addEventListener("fetch", (event) => {
+  
+  //   console.warn('url',event.request.url);
+  if (!navigator.onLine) {
+    if (event.request.url === "htps://localhost:3000/static/js/main.chunk.js") {
     event.waitUntil(
-        caches.open(cacheData).then((cache)=>{
-           return  cache.addAll([
-                '/static/js/main.chunk.js',
-                '/static/js/0.chunk.js',
-                '/static/js/bundle.js',
-                '/index.html',
-                '/',
-                '/about',
-                '/users'
-
-            ])
-        })
-    )
-})
-
-this.addEventListener("fetch",(event)=>{
-   if(!navigator.onLine){
+      this.Registration.showNotification("hello user", {
+        body: "internet not working properly",
+      })
+    );
+  }
     event.respondWith(
-        caches.match(event.request).then((response)=>{
-            if(response){
-                return response
-            }
-            let requestUrl = event.request.clone();
-            return fetch(requestUrl)
-        })
-    )
-   }
-})
+      caches.match(event.request).then((response) => {
+        if (response) {
+          return response;
+        }
+        let requestUrl = event.request.clone();
+        return fetch(requestUrl);
+      })
+    );
+  }
+});
--- fisrt-pwa/src/SwDev.js
+++ fisrt-pwa/src/SwDev.js
@@ -1,10 +1,37 @@
 import React from 'react'
 
 const SwDev = () => {
+  function determineAppServerKey() {
+    const vapidPublicKey =
+    "BJthRQ5myDgc7OSXzPCMftGw-n16F7zQBEN7EUD6XxcfTTvrLGWSIG7y_JxiWtVlCFua0S8MTB5rPziBqNx1qIo";
+      return urlBase64ToUint8Array(vapidPublicKey);
+  }
+
+  function urlBase64ToUint8Array(base64String) {
+    const padding = '='.repeat((4 - base64String.length % 4) % 4);
+    const base64 = (base64String + padding)
+      .replace(/\-/g, '+')
+      .replace(/_/g, '/');
+
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+
+    for (let i = 0; i < rawData.length; ++i) {
+      outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+  }
+
     let swURL = `${process.env.PUBLIC_URL}/sw.js`
-    navigator.serviceWorker.register(swURL).then((res)=>{
-        console.warn('response ',res)
-    })
+    navigator.serviceWorker.register(swURL).then((response)=>{
+        console.warn('response ',response)
+        return response.pushManager.getSubscription()
+        .then(function (subscription) {
+           response.pushManager.subscribe({
+            userVisibleOnly: true,
+            applicationServerKey: determineAppServerKey()
+          })
+    })});
   return (
     <div>SwDev</div>
   )
